80811d902a513f635387342baadfd1a461d8534f
tandavala <jose.tandavala@gmail.com>
2019-05-18 03:32:01 +0100

    giving a description to our teste

--- tests/Feature/ViewBlogPost.php
+++ tests/Feature/ViewBlogPost.php
@@ -10,6 +10,16 @@ class ViewBlogPost extends TestCase
 {
   public function testCanViewABlogPost()
   {
-      
+      // Arrangement
+      // creating a blog post
+
+      // Action
+      //visiting a route
+
+      // Assert
+      // assert status 200
+      // assert that we see post title
+      // assert that we see post body
+      // assert that we see pubished data
   }
 }
