3ec7a5677d37f9bd61a0d8b6526c1b03de798c30
tandavala <jose.tandavala@gmail.com>
2019-05-18 03:40:00 +0100

    running red create post teste

--- tests/Feature/ViewBlogPostTest.php
+++ tests/Feature/ViewBlogPostTest.php
@@ -6,20 +6,28 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ViewBlogPost extends TestCase
+class ViewBlogPostTest extends TestCase
 {
   public function testCanViewABlogPost()
   {
       // Arrangement
       // creating a blog post
-
+      $post = Post::create([
+          'title' => 'Simple title',
+          'body' => 'Simple body'
+      ]);
       // Action
       //visiting a route
+      $resp = $this->get("/post/{$post->id}");
 
       // Assert
       // assert status 200
+      $resp->assertStatus(200);
       // assert that we see post title
+      $resp->assertSee($post->title);
       // assert that we see post body
+      $resp->assertSee($post->body);
       // assert that we see pubished data
+      $resp->assertSee($post->created_at);
   }
 }
